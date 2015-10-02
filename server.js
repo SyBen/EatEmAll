81749b574950cf08285dd9fd84fb1fe240a61436
blobby <djee.aie@gmail.com>
2015-10-02 09:05:11 +0200

    better server + use socket.io

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // Imports
-var http = require("http");
+var server = require("http").createServer(handler);
+var socketio = require("socket.io");
+var fs = require("fs");
 // Program vars
 
+function handler (req, res) {
+  fs.readFile(__dirname + '/site/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-var server = http.createServer(function(req, res) {
-  res.writeHead("200");
-  res.end("Hello there");
+    res.writeHead(200);
+    res.end(data);
+});
   console.log("Connection from : " + req.connection.remoteAddress + " with headers : ");
   console.log(req.headers);
-});
+}
 
 server.listen(8080);
