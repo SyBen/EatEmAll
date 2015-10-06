77f0f068b74f4b80b66bf991e99f948e65c87590
Le Goff Quentin <lg.quentin@gmail.com>
2015-10-06 19:55:47 +0200

    add dependencies to bower.json and package.json + use of jquery, bootstrap librairies on client

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-// Imports
 var fs = require("fs");
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var io = require("socket.io")(server);
-
-// Program vars
 var port = 8080;
+
+/******************
+Server
+******************/
 
 app.use(express.static('public'));
 
 app.get("/", function(req, res){
-   res.sendfile('index.html')
+   res.sendfile('index.html');
 });
 
-io.on("connection", function (socket) {
-  console.log("Socket connection");
+/******************
+Communication
+******************/
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function (socket) {
+  console.log("Socket connected");
 });
+
+
+
 
 server.listen(port);
-console.log("Listening on port " + port);
