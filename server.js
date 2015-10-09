344ee9b10cad7c93fc0b9ffcc59e90b61efe5937
Le Goff Quentin <lg.quentin@gmail.com>
2015-10-09 16:48:25 +0200

    merge

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var fs = require("fs");
 var express = require("express");
 var app = express();
-var server = require("http").createServer(app);
 var port = 8080;
+var server = require("http").createServer(app);
 
 /******************
 Server
@@ -10,8 +9,8 @@ Server
 
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
-   res.sendfile('index.html');
+app.get("/", function (req, res) {
+  res.sendfile('index.html');
 });
 
 /******************
@@ -19,11 +18,13 @@ Communication
 ******************/
 var io = require("socket.io").listen(server);
 
-io.sockets.on("connection", function (socket) {
+io.on("connection", function (socket) {
   console.log("Socket connected");
 });
 
 
 
 
+//Run server
 server.listen(port);
+console.log("Server runs on port : " + port);
