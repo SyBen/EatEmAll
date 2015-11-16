3448685f657e83025edf91949388082dac4b8ddc
SyBen <benhamiche.sylvain@gmail.com>
2015-11-16 17:12:03 +0100

    started to implement grid

--- app/models/game.js
+++ app/models/game.js
@@ -1,14 +1,14 @@
 define(['app/models/player'], function (Player) {
   'use strict';
 
-  function Game() {
+  function Game(xSize, ySize) {
 
     if (!(this instanceof Game)) {
       throw new TypeError('Game constructor cannot be called as a function.');
     }
 
     this.players = [];
-
+    this.grid = [xSize][ySize];
   }
 
   Game.prototype = {
@@ -18,29 +18,31 @@ define(['app/models/player'], function (Player) {
     addPlayer: function (data) {
       var player = new Player(data);
       this.players.push(player);
-      
+
+      grid[player.getXPosition][player.getYPosition] = this.players.indexOf(player);
+
       return this.players.indexOf(player);
     },
 
     removePlayer: function (player) {
       this.players.splice(this.players.indexOf(player));
     },
-    
+
     getPlayers: function () {
       return this.players;
-    },    
-    
+    },
+
     getPlayerById: function (playerId) {
       return this.players[playerId];
     },
-    
+
     setPlayerPosition: function (currentPlayerId, direction) {
       var currentPlayer = this.getPlayerById(currentPlayerId);
-      var possible = true;        
-        
-      if(direction === 'left'){ 
-        console.log(currentPlayer.getNickname() + ' veut aller à gauche.');   
-      
+      var possible = true;
+
+      if(direction === 'left'){
+        console.log(currentPlayer.getNickname() + ' veut aller à gauche.');
+
         this.getPlayers().forEach( function (otherPlayer, otherPlayerId) {
           if(otherPlayerId != currentPlayerId){
             if((otherPlayer.getXPosition() === currentPlayer.getXPosition()-1)&&(otherPlayer.getYPosition() === currentPlayer.getYPosition())){
@@ -48,14 +50,14 @@ define(['app/models/player'], function (Player) {
             }
           }
         });
-        
+
         if(possible)
           currentPlayer.setXPosition(currentPlayer.getXPosition() - 1);
 
       }
       else if(direction === 'right'){
-        console.log(currentPlayer.getNickname() + ' veut aller à droite.');   
-      
+        console.log(currentPlayer.getNickname() + ' veut aller à droite.');
+
         this.getPlayers().forEach( function (otherPlayer, otherPlayerId) {
           if(otherPlayerId != currentPlayerId){
             if((otherPlayer.getXPosition() === currentPlayer.getXPosition()+1)&&(otherPlayer.getYPosition() === currentPlayer.getYPosition())){
@@ -63,15 +65,15 @@ define(['app/models/player'], function (Player) {
             }
           }
         });
-        
+
         if(possible)
           currentPlayer.setXPosition(currentPlayer.getXPosition() + 1);
-        
- 
+
+
       }
       else if(direction === 'up'){
-        console.log(currentPlayer.getNickname() + ' veut aller en haut.');   
-      
+        console.log(currentPlayer.getNickname() + ' veut aller en haut.');
+
         this.getPlayers().forEach( function (otherPlayer, otherPlayerId) {
           if(otherPlayerId != currentPlayerId){
             if((otherPlayer.getYPosition() === currentPlayer.getYPosition()+1)&&(otherPlayer.getXPosition() === currentPlayer.getXPosition())){
@@ -79,16 +81,16 @@ define(['app/models/player'], function (Player) {
             }
           }
         });
-        
+
         if(possible)
           currentPlayer.setYPosition(currentPlayer.getYPosition() + 1);
-        
+
 
       }
       else if(direction === 'down'){
         console.log(currentPlayer.getNickname() + ' veut aller en bas.');
 
-      
+
         this.getPlayers().forEach( function (otherPlayer, otherPlayerId) {
           if(otherPlayerId != currentPlayerId){
             if((otherPlayer.getYPosition() === currentPlayer.getYPosition()-1)&&(otherPlayer.getXPosition() === currentPlayer.getXPosition())){
@@ -96,17 +98,17 @@ define(['app/models/player'], function (Player) {
             }
           }
         });
-        
+
         if(possible)
           currentPlayer.setYPosition(currentPlayer.getYPosition() - 1);
-        
-       
+
+
       }
-      console.log(currentPlayer.getXPosition() + ', ' + currentPlayer.getYPosition());  
-      
+      console.log(currentPlayer.getXPosition() + ', ' + currentPlayer.getYPosition());
+
       return possible;
     },
-    
+
   };
 
   return Game;
